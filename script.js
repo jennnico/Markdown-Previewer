9ed749b887a32ab276ac2a63cc2c230911057d0e
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-07-31 17:26:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ class Previewer extends React.Component{
   constructor(){
     super();
     this.state = {
-      heading: "Here is a heading!",
-      subheading: "Here is a subheading"
+        heading: "Here is a heading!",
+        subheading: "Here is a subheading"
+        
     };
   }
   
+  changeHeading(heading){
+    this.setState({ heading: heading});
+  }
+  
+  handleChange(e){
+    const heading = e.target.value;
+    this.props.changeHeading(heading);
+  }
+  
   render(){
-    const {heading, subheading} = this.state;
+    const {subheading} = this.state;
     return (
       <div>
         <h2 class="headings">Editor</h2>
-          <textarea id="editor" >{subheading}</textarea>
+          <textarea id="editor" onChange={(event) => this.changeHeading(event.target.value)}></textarea>
         <h2 class="headings">Previewer</h2>
         <div id="preview">
-          <h1>{heading}</h1>
+          <h1>{this.state.heading}</h1>
           <h2>{subheading}</h2>
         </div>
       </div>
